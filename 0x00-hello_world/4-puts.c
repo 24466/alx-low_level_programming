9cbfe01e7770d8b2c2044554c5f2745dd9214fab
24466 <wanjikungocho@gmail.com>
2022-09-12 01:37:23 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 
-
 /**
  *
  *  * main - Entry point
@@ -11,13 +10,9 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
