baf49926c31069f60de279dae3e3983d6d6a6e94
24466 <wanjikungocho@gmail.com>
2022-09-15 04:20:33 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10x
+ *
+ * Return: always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int j, i;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
