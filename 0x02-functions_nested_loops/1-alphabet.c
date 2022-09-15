22a1bb651ac2f96bfbdad3843a729223ed2c8532
24466 <wanjikungocho@gmail.com>
2022-09-15 04:06:21 -0700

    alphabet lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
+
 /**
  * print_alphabet - print alphabet in lowercase
- *
- * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
@@ -14,5 +13,4 @@ void print_alphabet(void)
 		_putchar(a);
 	}
 	_putchar('\n')
-		return (0);
 }
