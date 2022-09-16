a7b87c8b1177f2955bab3fea52fd634f4a6f53e0
24466 <wanjikungocho@gmail.com>
2022-09-16 02:21:37 -0700

    prints the numbers, from 0 to 9 skip 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0-9 7 skip 2 & 4
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 && a != 52)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n')
+}
