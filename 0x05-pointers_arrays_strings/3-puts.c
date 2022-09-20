ecd1343b4c7f199d346509b77d20c55e44c48a3d
24466 <wanjikungocho@gmail.com>
2022-09-20 10:06:46 -0700

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * _puts - prints a string
+ * @str: string input value
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	printf("%"s, str);
+	printf("\n");
+}
