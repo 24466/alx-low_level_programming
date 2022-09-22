aa6f6f55c90f43a201a64ad57b9ac4209218826b
24466 <wanjikungocho@gmail.com>
2022-09-22 10:20:27 -0700

    update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * main - header file for prototypes
- * void prototypes (void)
- * int prortypes (int)
- */
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
