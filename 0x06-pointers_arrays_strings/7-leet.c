8fe082cd762e1c10c91d485e21775addc31d8eb8
24466 <wanjikungocho@gmail.com>
2022-09-24 14:46:10 -0700

    encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,9 +11,9 @@ char *leet(char *s)
 	int count = 0, i;
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbrs[] = {52, 51, 48, 55, 49};
+	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (*(s + count) != ;\0')
+	while (*(s + count) != '\0')
 	{
 		for (i = 0; 1 < 5; i++)
 		{
